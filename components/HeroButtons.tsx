1b8b595f481c2bc43f2d7f656f52240eb06141fb
Shorno <fb.shorno@gmail.com>
2024-07-04 21:58:09 +0600

    added hero buttons

---
+++ components/HeroButtons.tsx
@@ -0,0 +1,24 @@
+"use client";
+import React from "react";
+import {HoverBorderGradient} from "@/components/ui/hover-border-gradient";
+import {FaGithub} from "react-icons/fa";
+import {TbBrandOauth} from "react-icons/tb";
+
+export function HeroButtons() {
+    return (
+        <div className="m-20 flex flex-col gap-10 justify-center text-center md:flex-row">
+            <HoverBorderGradient
+                containerClassName="rounded-xl w-full md:w-auto"
+                as="button"
+                className="dark:bg-black bg-white text-black dark:text-white flex items-center space-x-2"
+            >
+                <span className={"flex justify-center items-center gap-2"}> <TbBrandOauth size={25}/> Get Started</span>
+            </HoverBorderGradient>
+            <button
+                className="inline-flex gap-2 h-12 animate-shimmer items-center justify-center rounded-xl border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-1 focus:ring-slate-400 focus:ring-offset-1 focus:ring-offset-slate-50">
+                <FaGithub/> Code
+            </button>
+
+        </div>
+    );
+}
--- components/HeroHighlightText.tsx
+++ components/HeroHighlightText.tsx
@@ -1,6 +1,7 @@
 "use client";
 import {motion} from "framer-motion";
 import {HeroHighlight, Highlight} from "@/components/ui/hero-highlight";
+import {HeroButtons} from "@/components/HeroButtons";
 
 
 export default function HeroHighlightText() {
@@ -19,17 +20,19 @@ export default function HeroHighlightText() {
                     duration: 0.5,
                     ease: [0.4, 0.0, 0.2, 1],
                 }}
-                className="text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto "
+                className="text-lg px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto "
             >
-                <div className="p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
-                    <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
-                        EasyAuth <br/>the modern way to
+                <div className="p-4 max-w-7xl  mx-auto relative z-10 w-full pt-20 md:pt-0">
+                    <h1 className="text-5xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+                        EasyAuth <br/> <span className={"text-3xl sm:text-7xl"}>the modern way to</span>
                     </h1>
                 </div>
-                <Highlight className="text-black dark:text-white">
-                    build easy, robust authentication
+                <Highlight className="">
+                    build fast and secure authentication
                 </Highlight>
             </motion.h1>
+            <HeroButtons/>
+
         </HeroHighlight>
     );
 }
