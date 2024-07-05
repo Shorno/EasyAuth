0a1c50d9280642e4e774d0df3164127a3140430f
Shorno <fb.shorno@gmail.com>
2024-07-05 11:04:14 +0600

    prisma edge

--- auth.ts
+++ auth.ts
@@ -1,14 +1,13 @@
 import NextAuth from "next-auth"
 import {PrismaAdapter} from "@auth/prisma-adapter";
 import GitHub from "@auth/core/providers/github";
-import {Adapter} from "@auth/core/adapters";
-import {PrismaClient} from "@prisma/client";
+import {PrismaClient} from '@prisma/client/edge'
 
 const prisma = new PrismaClient()
 
 
 export const {handlers, auth, signIn, signOut} = NextAuth({
-    adapter: <Adapter>PrismaAdapter(prisma),
+    adapter: PrismaAdapter(prisma),
     providers: [
         GitHub({
             clientId: process.env.GITHUB_CLIENT_ID,
