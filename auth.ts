3a194e44d4ff1e9ee8617281ce37f830f2ea5bf5
Shorno <fb.shorno@gmail.com>
2024-07-05 11:17:47 +0600

    ....

--- auth.ts
+++ auth.ts
@@ -1,7 +1,11 @@
 import NextAuth from "next-auth"
 import {PrismaAdapter} from "@auth/prisma-adapter";
 import GitHub from "@auth/core/providers/github";
-import {prisma} from "@/prisma/db";
+import {PrismaClient} from '@prisma/client'
+
+
+const prisma = new PrismaClient()
+
 
 export const {handlers, auth, signIn, signOut} = NextAuth({
     adapter: PrismaAdapter(prisma),
